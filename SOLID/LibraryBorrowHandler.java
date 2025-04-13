a734ebb0355cdd0640c5d433fcaa0a0aa4037bf3
Cedric Bandong <83577761+CedricBandong@users.noreply.github.com>
2025-04-13 21:12:35 +0800

    LabAss6

---
+++ SOLID/LibraryBorrowHandler.java
@@ -0,0 +1,5 @@
+public class LibraryBorrowHandler implements BorrowHandler {
+    public void handleBorrow(String studentName, Resource resource) {
+        System.out.println(studentName + " borrowed a " + resource.getType() + " titled '" + resource.getTitle() + "'.");
+    }
+}
